e55e43c12833266f1d4fa19bfe0cd37188597241
junaidi <junaidi@bsi.co.id>
2022-11-13 12:22:24 +0700

    fix docker compose sample for asp.net webapi. It still bug in swagger

--- examples/07-net-core-in-docker-compose/MyWebApi/Program.cs
+++ examples/07-net-core-in-docker-compose/MyWebApi/Program.cs
@@ -35,4 +35,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//https://www.hanselman.com/blog/exploring-a-minimal-web-api-with-aspnet-core-6
+//https://khalidabuhakmeh.com/simple-redirects-with-aspnet-core-endpoint-routing
+
+app.MapGet("/", ctx =>
+{
+    ctx.Response.Redirect("/swagger", true);
+    return Task.CompletedTask;
+});
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+
+//    endpoints.MapGet("/", ctx => { 
+//        ctx.Response.Redirect("/swagger", true); 
+//        return Task.CompletedTask; 
+//    });
+//});
+
+
 app.Run();
---
+++ examples/07-net-core-in-docker-compose/Readme.md
@@ -0,0 +1,13 @@
+1. Build docker compose
+    ```
+    docker-compose build
+    ```
+2. Build container
+    ```
+    docker-compose up
+    ```
+
+3. Test open API
+    ```
+    http://localhost:8000/Blog
+    ```
